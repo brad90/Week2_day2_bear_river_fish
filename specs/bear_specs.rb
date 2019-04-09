42ed38dff7283360a76452f86ab0f2501a7d2f01
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-09 23:14:13 +0100

    adding more tests to bear

--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -8,21 +8,39 @@ require_relative('../river.rb')
 class TestBear < MiniTest::Test
 
   def setup
-    @bear1 = Bear.new("Yogi","Grizzly")
+    @bear1 = Bear.new("Yogi","Grizzly",)
+    @bear2 = Bear.new("TopCat","Cat",)
+    @bear3 = Bear.new("Yogi","Grizzly",["fish","fish","fish"])
     @river1 = River.new("Amazon", ["bob","chad"])
 
   end
 #this is correct
   def test_what_is_the_bears_name
-    @bear1.name
+   p @bear1.name
+   assert_equal("Yogi", @bear1.name)
   end
+
 #this is correct
   def test_what_is_the_bear_type
     assert_equal("Grizzly", @bear1.type)
   end
+
+
 #this is correct
-  def test_does_the_bear_roar
+  def test_does_the_bear_roar__does
     assert_equal("Roooarrr",@bear1.does_bear_roar)
+  end
+
+  def test_does_the_bear_roar__not
+    assert_equal("I dont roar...sad!", @bear2.does_bear_roar)
+  end
+
+  def test_is_bear_hungry__less_2_fish
+    assert_equal(true,@bear1.is_bear_hungry)
+  end
+
+  def test_is_bear_hungry__more_than_2_fish
+    assert_equal(false,@bear3.is_bear_hungry)
   end
 #this is correct
   def test_eating_fish_in_river
@@ -30,7 +48,7 @@ class TestBear < MiniTest::Test
     assert_equal(1,@bear1.food_in_belly_count)
   end
 
-  
+
 
 
 end
